95019491f6a613181ec7b90795d9d2a8ab987e83
divide10 <divide10@outlook.com>
2014-07-10 23:04:09 +0800

    new blog

---
+++ _posts/2014-07-10-STAP-for-radar-book-organization.markdown
@@ -0,0 +1,10 @@
+---
+date = 2014-07-10
+layout = post
+categories = STAP
+title = STAP for Radar-Book Organization
+---
+##缘起##
+由于研究的需要，开始阅读J.R.Guerci的*Space-Time Adaptive Processing for Radar*。本书一共有6个章节，为以后阅读的方便性和目标性，在此将第一章Introduction中的Book Organization进行摘录。
+
+##第二章##
---
+++ _posts/initmk.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python 
+import time
+import subprocess
+headdic = {}
+headdic["layout"] = "post"
+headdic["date"] = time.strftime("%Y-%m-%d",time.localtime(time.time()))
+nametitle = raw_input("File name (Example ***-***-):\n")
+headdic["title"] = raw_input("Blog title:\n")
+headdic["categories"] = raw_input("Categories:\n")
+
+text ='---\n'+"\n".join(["%s = %s" %(k,v) for k,v in headdic.items()]) + '\n---'
+mkname = headdic["date"] + '-'+nametitle+'.markdown'
+
+f = open(mkname,'w+') 
+f.write(text)
+f.close()
+subprocess.call(["subl",mkname])
